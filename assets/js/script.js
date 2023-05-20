d67e756a9b72a4ffe8e6fa49b0afb3fdf7f9ce79
Dusty Brigsby <108590519+dustybrigsby@users.noreply.github.com>
2023-05-19 17:35:45 -0700

    Added commented structure outline to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,18 @@
-// Assignment Code
+// Create arrays of different types of characters
+
+// Uppercase letters
+// Lowercase letters
+// Numbers
+// Symbols
+
+// Ask for user's desired password length between 8-12 characters
+  // Check length and that it's a number
+
+// Ask if user wants to include each of the four types of characters
+  // Check that at least one option is selected
+
+// Generate password based off choices when button clicked
+
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
