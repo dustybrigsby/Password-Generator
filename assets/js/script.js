97837bae830b0985de6d29788eb9b144b1f4a28c
Dusty Brigsby <108590519+dustybrigsby@users.noreply.github.com>
2023-05-21 07:54:02 -0700

    Update script.js
    Added the user's choices as confirm messages, error catches, tryAgain function, and get random array element function

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ const numberCharacters = [
   "9",
 ];
 // Symbols
-const symbolCharacters = [
+const specialCharacters = [
   "`",
   "~",
   "!",
@@ -111,7 +111,15 @@ const symbolCharacters = [
 
 // Generate password based off choices when button clicked
 
-var generateBtn = document.querySelector("#generate");
+// If user choice didn't meet requirements, this asks if they would like to try again or not
+function tryAgain(choice) {
+  if (choice) {
+    userChoices();
+  } else {
+    alert("Generator canceled, refresh the page to start again.");
+    return null;
+  }
+}
 
 // Prompts for user to make selections for each option
 function userChoices() {
@@ -123,17 +131,92 @@ function userChoices() {
 
   // Checks if length is a number
   if (Number.isNaN(length)) {
-    let tryAgain = confirm(
-      "Password length must be a number.\nTry again?"
-    );
-
-    if (tryAgain) {
-      userChoices();
-    } else {
-      return null;
-    }
+    let choice = confirm("Password length must be a number.\nTry again?");
+    tryAgain(choice);
   }
+
+  // Checks length meets minimum requirements of >= 8
+  if (length < 8) {
+    let choice = confirm(
+      "Password must be at least 8 characters long.\nTry again?"
+    );
+    tryAgain(choice);
+  }
+
+  // Checks length meets maximum requirements of <= 128
+  if (length > 128) {
+    let choice = confirm(
+      "Password must be less than 128 characters long.\nTry again?"
+    );
+    tryAgain(choice);
+  }
+
+  // Ask if password should include uppercase letters
+  var hasUpperLetters = confirm(
+    "Click OK to include uppercase letters in your password.\nClick Cancel to not include uppercase letters."
+  );
+
+  // Ask if password should include lowercase letters
+  var hasLowerLetters = confirm(
+    "Click OK to include lowercase letters in your password.\nClick Cancel to not include lowercase letters."
+  );
+
+  // Ask if password should include numbers
+  var hasNumbers = confirm(
+    "Click OK to include numbers in your password.\nClick Cancel to not include numbers."
+  );
+
+  // Ask if password should include symbols
+  var hasSpecial = confirm(
+    "Click OK to include special characters in your password.\nClick Cancel to not include special characters."
+  );
+
+  // Check that at least one option was selected by the user
+  if (
+    hasUpperLetters === false &&
+    hasLowerLetters === false &&
+    hasNumbers === false &&
+    hasSpecial === false
+  ) {
+    let choice = confirm(
+      "At least one type of character must be selected.\nTry again?"
+    );
+    tryAgain(choice);
+  }
+
+  // Stores user choices into an object
+  var choices = {
+    length: length,
+    hasUpperLetters: hasUpperLetters,
+    hasLowerLetters: hasLowerLetters,
+    hasNumbers: hasNumbers,
+    hasSpecial: hasSpecial,
+  };
+  return choices;
 }
+
+// Gets random element from input array
+function getRandom(arr) {
+  var index = Math.floor(Math.random() * arr.length);
+  var randomElement = arr[index];
+
+  return randomElement;
+}
+
+// Generates password based off user choices
+function generatePassword() {
+  var choices = userChoices();
+
+  // Check user choices to make sure they exist
+  if (!choices) {
+    alert("No options selected, please refresh page to start again.");
+    return null;
+  }
+
+  
+}
+
+var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
